fb9163624bd604bd565083e3e72621feeadc3409
David Bean <dbean23@gmail.com>
2019-09-04 22:03:20 -0600

    built formbutton component in formfiels.js and addend submit/login button to signinForm

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 
 export class FormInput extends Component {
-    render () {
+    render() {
         const { className, title, type, placeholder, input } = this.props;
         return (
             <div className={`${className} form-input`}>
-                <label className='form-input__lable'>{title}</label>
+                <label className='form-input__label'>{title}</label>
                 <input className='form-input__input' type={type} placeholder={placeholder} {...input} />
             </div>
-        );
+        )
+    }
+}
+
+export class FormButton extends Component {
+    render() {
+        const { className, title, type, onClick, input, short } = this.props;
+        return (
+            <div className={`${className} form-button`}>
+                <button className={`form-button__button`}
+                        type={type}
+                        {...input}
+                        onClick={onClick}
+                >
+                {title}
+                </button>
+            </div>
+        )
     }
 }
