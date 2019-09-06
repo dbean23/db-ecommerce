c79bc910ec848fce639a601e2cd27f1a200ea8f0
David Bean <dbean23@gmail.com>
2019-09-05 20:52:44 -0600

    finished signUp Form and grid

--- src/components/auth/signupForm.js
+++ src/components/auth/signupForm.js
@@ -13,21 +13,18 @@ import history from '../../history';
 class SignUpForm extends Component {
     render () {
         const { className, handleSubmit } = this.props;
-        const links = [
+        const info = [
             {
                 _id: 0,
-                title: 'Not registered? Create account here',
-                onClick: () => history.push('/signup')
+                title: 'At least 6 characters',
             },
             {
                 _id: 1,
-                title: 'Forgot account email?',
-                onClick: () => console.log('forgot email')
+                title: 'At least one number',
             },
             {
                 _id: 2,
-                title: 'Forgot password?',
-                onClick: () => console.log('forgot password')
+                title: 'At least one symbol',
             }
         ]
         return (
@@ -50,7 +47,7 @@ class SignUpForm extends Component {
                     placeholder='Password'
                     name='password'
                     component={FormInput} /> 
-                <Field className='sign-up-form__confirm-password'
+                <Field className='sign-up-form__confirm'
                     type='password'
                     title='Confirm Password'
                     name='confirm'
@@ -59,13 +56,13 @@ class SignUpForm extends Component {
 
                 <div className='sign-in-form__line'></div>   
                 <Field className='sign-in-form__login'
-                    onClick={() => console.log('tryna submit')}
+                    onClick={() => history.push('/account')}
                     type='submit'
-                    title='Login'
-                    name='login'
+                    title='Create Account'
+                    name='create account'
                     component={FormButton} />   
                 <Field className='sign-in-form__back'
-                    onClick={() => console.log('tryna submit')}
+                    onClick={() => history.push('/signin')}
                     type='button'
                     title='Back'
                     name='back'
@@ -73,7 +70,7 @@ class SignUpForm extends Component {
                     component={FormButton} />                       
 
 
-                <Details className='sign-up-form__details' title='QuickLinks' links={links}/>                  
+                <Details className='sign-up-form__details' title='Password Requirements' info={info}/>                  
             </form>
         );
     }
--- src/components/details.js
+++ src/components/details.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 
 export default class Details extends Component {
+
+    renderChildren=( ) => {
+        const {links, info } =this.props;
+        let children = [];
+        if(links) {
+            children = links.map((link) => {
+                return <a key={link._id} onClick={link.onClick} className='details__link'>{link.title}</a>
+            })
+        } else if (info) {
+            children = info.map((item) => {
+                return <div key={item._id} onClick={item.onClick} className='details__item' >{item.title}</div>
+            })
+            
+        }
+        return children;
+    }
     render () {
-        const { title, links, className } = this.props;
+
+        const { title, className } = this.props;
         return (
             <div className={`${className} details`}>
                 <div className='details__title'>{title}</div>
                 <div className='details__links'>
                     {
-                        links.map((link) => {
-                            return <a key={link._id} onClick={link.onClick} className='details__link'>{link.title}</a>
-                        })
+                        this.renderChildren()
+
                     }
                 </div>
             </div>
